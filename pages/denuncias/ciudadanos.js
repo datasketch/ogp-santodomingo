3e0fe91ab154197c5e8b2f57132654411b146f7b
David Daza <03dazal@gmail.com>
2022-09-02 11:54:37 -0500

    Add react-hook-form to handle submit data

---
+++ pages/denuncias/ciudadanos.js
@@ -0,0 +1,197 @@
+import { Box, Button, Checkbox, FormControl, FormErrorMessage, FormHelperText, FormLabel, Heading, Input, Select, Stack, Textarea } from '@chakra-ui/react'
+import dynamic from 'next/dynamic'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import isEmail from 'validator/lib/isEmail'
+import { complainantTypes, complaintTypes, componentsList, dictionary, parishList, sectorsList } from '../../utils'
+
+const Map = dynamic(() => import('../../components/Map'), {
+  ssr: false,
+})
+
+function CitizenFormPage () {
+  const center = { lat: -0.254167, lng: -79.1719 }
+
+  const { handleSubmit, register, formState: { errors } } = useForm({
+    mode: 'onBlur',
+  })
+
+  const [tipoDenunciante, setTipoDenunciante] = useState('')
+  const [coordinates, setCoordinates] = useState(`${center.lat}, ${center.lng}`)
+
+  const onSubmit = data => {
+    const info = {
+      ...data,
+      [dictionary.ubicacion]: coordinates
+    }
+    console.log(info);
+  }
+
+  return (
+    <Box
+      width="100%"
+      maxWidth={768}
+      px={16}
+      py={4}
+      mx="auto"
+    >
+      <Heading size="lg">Formulario de denuncias - Ciudadanos</Heading>
+      <form action="" onSubmit={handleSubmit(onSubmit)}>
+        <Stack dir="column" spacing={5} mt={5}>
+          <FormControl isRequired>
+            <FormLabel>Tipo de denunciante</FormLabel>
+            <Select
+              placeholder='Seleccione una opción'
+              {...register(dictionary.tipoDenunciante, {
+                onChange: e => setTipoDenunciante(e.target.value),
+              })}
+            >
+              {complainantTypes.map(option => (
+                <option value={option} key={option}>{option}</option>
+              ))}
+            </Select>
+          </FormControl>
+          {tipoDenunciante === 'Persona Natural' && (
+            <FormControl>
+              <FormLabel>Nombres y Apellidos</FormLabel>
+              <Input {...register(dictionary.nombresApellidos)} />
+            </FormControl>
+          )}
+          <FormControl>
+            <FormLabel>C.I. / RUC</FormLabel>
+            <Input {...register(dictionary.identificador)} />
+          </FormControl>
+          {tipoDenunciante && tipoDenunciante !== 'Persona Natural' && (
+            <FormControl>
+              <FormLabel>Razón social</FormLabel>
+              <Input {...register(dictionary.razonSocial)} />
+            </FormControl>
+          )}
+          <FormControl
+            isInvalid={errors && errors[dictionary.email]}
+            isRequired
+          >
+            <FormLabel>Email</FormLabel>
+            <Input type="email" {...register(dictionary.email, {
+              validate: v => isEmail(v) || 'Por favor introduzca un email válido'
+            })} />
+            { errors && errors[dictionary.email] && (
+                <FormErrorMessage>
+                  {errors[dictionary.email].message}
+                </FormErrorMessage>
+              )
+            }
+          </FormControl>
+          <FormControl>
+            <FormLabel>Teléfono</FormLabel>
+            <Input {...register(dictionary.telefono)} />
+          </FormControl>
+          <FormControl isRequired>
+            <FormLabel>Fecha del incidente</FormLabel>
+            <Input type="datetime-local" {...register(dictionary.fechaIncidente)} />
+          </FormControl>
+          <FormControl isRequired>
+            <FormLabel>Tipo de denuncia</FormLabel>
+            <Select
+              placeholder='Seleccione una opción'
+              {...register(dictionary.tipoDenuncia)}
+            >
+              {complaintTypes.map(option => (
+                <option value={option} key={option}>{option}</option>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl isInvalid={errors && errors[dictionary.componenteAfectado]}>
+            <FormLabel>Componente afectado</FormLabel>
+            <Stack direction='row' spacing={3}>
+              {componentsList.map(option => (
+                <Checkbox
+                  {...register(dictionary.componenteAfectado, {
+                    validate: v => (v && v.length) || 'Seleccione al menos un componente'
+                  })}
+                  value={option}
+                  key={option}
+                >
+                  {option}
+                </Checkbox>
+              ))}
+            </Stack>
+            { errors && errors[dictionary.componenteAfectado] ? (
+                <FormErrorMessage>
+                  {errors[dictionary.componenteAfectado].message || errors[dictionary.componenteAfectado].root.message}
+                </FormErrorMessage>
+              ) : <FormHelperText>Seleccione al menos un componente</FormHelperText>
+            }
+          </FormControl>
+          <FormControl isRequired>
+            <FormLabel>Cantón</FormLabel>
+            <Select
+              placeholder='Seleccione una opción'
+              {...register(dictionary.canton)}
+            >
+              {['Santo Domingo', 'La Concordia'].map(option => (
+                <option value={option} key={option}>{option}</option>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl isRequired>
+            <FormLabel>Parroquia</FormLabel>
+            <Select
+              placeholder='Seleccione una opción'
+              {...register(dictionary.parroquia)}
+            >
+              {parishList.map(option => (
+                <option value={option} key={option}>{option}</option>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl isRequired>
+            <FormLabel>Sector</FormLabel>
+            <Select
+              placeholder='Seleccione una opción'
+              {...register(dictionary.sector)}
+            >
+              {sectorsList.map(option => (
+                <option value={option} key={option}>{option}</option>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl isRequired>
+            <FormLabel>Tipo de denunciado</FormLabel>
+            <Select
+              placeholder='Seleccione una opción'
+              {...register(dictionary.tipoDenunciado)}
+            >
+              {['Institución pública', 'Persona Jurídica', 'Persona Natural'].map(option => (
+                <option value={option} key={option}>{option}</option>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl isRequired>
+            <FormLabel>Nombre del denunciado</FormLabel>
+            <Input {...register(dictionary.nombreDenunciado)} />
+          </FormControl>
+          <FormControl>
+            <FormLabel>Descripción del acto que se denuncia</FormLabel>
+            <Textarea {...register(dictionary.descripcionDenuncia)} />
+          </FormControl>
+          <FormControl>
+            <FormLabel>Ubicación</FormLabel>
+            <Map
+              center={center}
+              onMarkerMove={(coords) => {
+                const { lat, lng } = coords
+                setCoordinates(`${lat}, ${lng}` )
+              }}
+            />
+          </FormControl>
+          <Button type='submit'>
+            Enviar
+          </Button>
+        </Stack>
+      </form>
+    </Box>
+  )
+}
+
+export default CitizenFormPage
---
+++ utils/index.js
@@ -0,0 +1,34 @@
+export const dictionary = {
+  tipoDenunciante: 'Tipo de denunciante',
+  nombresApellidos: 'Nombres y Apellidos',
+  identificador: 'CI / RUC',
+  razonSocial: 'Razón social',
+  email: 'Email',
+  telefono: 'Telefono',
+  tipoDenuncia: 'Tipo de denuncia',
+  estadoDenuncia: 'Estado de la denuncia',
+  fechaDenuncia: 'Fecha de denuncia',
+  inicialesFuncionarioReceptor: 'Iniciales funcionario receptor',
+  fechaIncidente: 'Fecha del incidente',
+  componenteAfectado: 'Componente afectado',
+  canton: 'Canton',
+  parroquia: 'Parroquia',
+  sector: 'Sector',
+  direccion: 'Direccion',
+  tipoDenunciado: 'Tipo de denunciado',
+  nombreDenunciado: 'Nombre del denunciado',
+  descripcionDenuncia: 'Descripcion del acto que se denuncia',
+  ubicacion: 'Ubicacion'
+}
+
+export const complainantTypes = ['Institución pública', 'Medio de comunicación', 'Persona Jurídica', 'Persona Natural']
+
+export const complaintTypes = ['Verbal', 'Escrita', 'Red social', 'Online']
+
+export const complaintStates = ['Denuncia recibida', 'Denuncia atendida', 'Denuncia archivada']
+
+export const parishList = ['Abraham Calazacón', 'Bombolí', 'Chiguilpe', 'Río Toachi', 'Río verde', 'Santo Domingo', 'Zaracay', 'Alluriquin', 'Puerto Limón', 'Luz de América', 'San Jacinto del Bua', 'Valle Hermoso', 'El Esfuerzo', 'Santa María del Toachi', 'La Concordia', 'Monterrey', 'La Villegas', 'Plan piloto']
+
+export const sectorsList = ['Urbano', 'Rural']
+
+export const componentsList = ['Aire', 'Agua', 'Suelo', 'Ruido']
