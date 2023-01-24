81c61128a88b9ef70e650da263c76bf74d6c83fb
LuisCalderonGit <luis2fcalderon@gmail.com>
2023-01-24 14:13:28 -0500

    update feature filter date and added other filter in page inventory

--- pages/api/inventory.js
+++ pages/api/inventory.js
@@ -15,14 +15,17 @@ export default async function handler (req, res) {
 
   try {
     let data = []
-    const { since, until } = query
+
+    const { since, sinceTrans, until, untilTrans } = query
 
     if (!since && !until) {
       data = await knex('inventario').where('Inventario', '>', 0)
     } else {
       const desde = since || format(new Date(null), 'yyyy-MM-dd')
       const hasta = until || format(new Date(), 'yyyy-MM-dd')
-      const rawResult = await knex.raw(rawQuery, [desde, hasta])
+      const desdeTrans = sinceTrans || format(new Date(null), 'yyyy-MM-dd')
+      const hastaTrans = untilTrans || format(new Date(), 'yyyy-MM-dd')
+      const rawResult = await knex.raw(rawQuery, [desdeTrans, hastaTrans, desde, hasta])
       data = rawResult.rows.filter(row => +row.Inventario > 0)
     }
 
--- pages/plantas/inventario.jsx
+++ pages/plantas/inventario.jsx
@@ -12,17 +12,24 @@ import { useState } from 'react'
 import queryString from 'query-string'
 
 function InventoryPage () {
-  const [selectedSinceDate, setSelectedSinceDate] = useState('')
-  const [selectedUntilDate, setSelectedUntilDate] = useState('')
-  const { data, error } = useSWR(['/api/inventory', selectedSinceDate, selectedUntilDate], (url, selectedSinceDate, selectedUntilDate) => axios.get(url + '?' + queryString.stringify({ since: selectedSinceDate, until: selectedUntilDate })).then(res => res.data))
+  const [selectedDeliveryDate, setSelectedDeliveryDate] = useState({ since: '', until: '' })
+  const [selectedTransplantingDate, setSelectedTransplantingDate] = useState({ sinceTrans: '', untilTrans: '' })
+  const { data, error } = useSWR(['/api/inventory', selectedDeliveryDate, selectedTransplantingDate], (url, selectedDeliveryDate, selectedTransplantingDate) =>
+    axios.get(url + '?' + queryString.stringify(({ ...selectedDeliveryDate, ...selectedTransplantingDate }))).then(res => res.data))
   const currentDate = format(new Date(), 'yyyy-MM-dd')
 
   const handleChange = (type, e) => {
     if (type === 'since') {
-      setSelectedSinceDate(e.target.value)
+      setSelectedDeliveryDate(prev => ({ ...prev, since: e.target.value }))
     }
     if (type === 'until') {
-      setSelectedUntilDate(e.target.value)
+      setSelectedDeliveryDate(prev => ({ ...prev, until: e.target.value }))
+    }
+    if (type === 'sinceTrans') {
+      setSelectedTransplantingDate(prev => ({ ...prev, sinceTrans: e.target.value }))
+    }
+    if (type === 'untilTrans') {
+      setSelectedTransplantingDate(prev => ({ ...prev, untilTrans: e.target.value }))
     }
   }
 
@@ -36,16 +43,41 @@ function InventoryPage () {
         <Heading color="gray.700">
           Inventario
         </Heading>
+
         <Box display="flex" columnGap={4}>
-          <Box alignItems="center" display="flex" columnGap={1}>
-            <Text flexShrink={0}>Fecha de entrega(desde): </Text>
-            <Input type="date" value={selectedSinceDate} max={currentDate} onChange={(e) => handleChange('since', e)} />
+
+          <Box>
+            <Text flexShrink={0}>Fecha de entrega </Text>
+            <Box display={'flex'} gap={2}>
+              <Box alignItems="center" display="flex" columnGap={1}>
+                <Text flexShrink={0}>desde : </Text>
+                <Input type="date" value={selectedDeliveryDate.since} max={currentDate} onChange={(e) => handleChange('since', e)} />
+              </Box>
+              <Box alignItems="center" display="flex" columnGap={1}>
+                <Text flexShrink={0}>hasta : </Text>
+                <Input type="date" value={selectedDeliveryDate.until} max={currentDate} onChange={(e) => handleChange('until', e)} />
+              </Box>
+            </Box>
           </Box>
-          <Box alignItems="center" display="flex" columnGap={1}>
-            <Text flexShrink={0}>Fecha de entrega (hasta): </Text>
-            <Input type="date" value={selectedUntilDate} max={currentDate} onChange={(e) => handleChange('until', e)} />
+
+          <Box >
+            <Text flexShrink={0}>Fecha de transplante </Text>
+            <Box display={'flex'} gap={2}>
+              <Box alignItems="center" display="flex" columnGap={1}>
+                <Text flexShrink={0}>desde : </Text>
+                <Input type="date" value={selectedTransplantingDate.since} max={currentDate} onChange={(e) => handleChange('sinceTrans', e)} />
+              </Box>
+              <Box alignItems="center" display="flex" columnGap={1}>
+                <Text flexShrink={0}>hasta : </Text>
+                <Input type="date" value={selectedTransplantingDate.until} max={currentDate} onChange={(e) => handleChange('untilTrans', e)} />
+              </Box>
+            </Box>
           </Box>
-          <DownloadCSV data={data} label='inventario' />
+
+          <Box>
+            <Text flexShrink={0}>&nbsp;</Text>
+            <DownloadCSV data={data} label='inventario' />
+          </Box>
 
         </Box>
       </Box>
